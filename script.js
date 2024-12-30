98b8d665f2bf3864b4754eaa4ced30b606271923
tahirtk <147325889+tahirtk@users.noreply.github.com>
2024-12-30 04:59:02 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const menu = document.getElementById('menu');
 toggle.addEventListener('click', () => {
   menu.classList.toggle('show');
 });
+
+function flipCard(switchElement) {
+  const card = switchElement.closest(".flip-card");
+  card.classList.toggle("flipped");
+
+  // Toggle the switch state (active/inactive)
+  switchElement.classList.toggle("active");
+}
