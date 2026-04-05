21aa55649b1549d1d3b1bc97352c911c1d6d9472
tahirtk <147325889+tahirtk@users.noreply.github.com>
2026-04-05 16:28:10 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -41,16 +41,56 @@ function sortTable(tableId, columnIndex) {
 function updateRanks(table) {
   const rows = table.rows;
   for (let i = 0; i < rows.length; i++) {
-    const rankCell = rows[i].cells[1]; // Rank cell
+    const rankCell = rows[i].cells[0]; // Rank cell
     rankCell.innerText = i + 1; // Update rank
   }
 }
 
 // Automatically sort tables on page load
 window.onload = function () {
-  sortTable("runsTable", 3);    // Sort Runs table by column 3
+  sortTable("runsTable", 2);    // Sort Runs table by column 3
   sortTable("wicketsTable", 3); // Sort Wickets table by column 3
-  sortTable("sixesTable", 3);   // Sort 6's table by column 3
-  sortTable("foursTable", 3);   // Sort 4's table by column 3
+  sortTable("sixesTable", 2);   // Sort 6's table by column 3
+  sortTable("foursTable", 2);   // Sort 4's table by column 3
 };
+
+
+const players = {
+  Hassan:    { matches: 22, runs: 197, six: 11, four: 13 },
+
+  Tahir:     { matches: 22, runs: 358, six: 26, four: 31 },
+
+  Faizan:    { matches: 18, runs: 189, six: 18, four: 8 },
+
+  Haider:    { matches: 21, runs: 162, six: 7, four: 9 },
+
+  Umer:      { matches: 24, runs: 116, six: 4, four: 6 },
+  
+  Ahmed:     { matches: 16, runs: 108, six: 3, four: 11 },
+
+  Faiz:      { matches: 8,  runs: 92,  six: 5, four: 7 },
+
+  Mohiyudin: { matches: 7,  runs: 51,  six: 2, four: 4 },
+
+  Arooj:     { matches: 3,  runs: 45,  six: 4, four: 3 },
+
+  Arham:     { matches: 11, runs: 33,  six: 2, four: 1 },
+};
+
+function showStats(name) {
+  const p = players[name];
+  
+  document.getElementById("cardName").innerText = name;
+  document.getElementById("cardMatches").innerText = + p.matches;
+  document.getElementById("cardRuns").innerText    =  + p.runs;
+  document.getElementById("cardSix").innerText     =  + p.six;
+  document.getElementById("cardFour").innerText      = + p.four;
+
+  document.getElementById("statsCard").style.display = "block";
+}
+
+function closeCard() {
+  document.getElementById("statsCard").style.display = "none";
+}
+
 
